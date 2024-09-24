6552f6eba7fd6b14a11d19e5503f36c478b6e8cc
Divya Bhuvanapalli <49796596+dbhuvanapalli@users.noreply.github.com>
2024-09-24 14:28:38 -0700

    Update setupscripts.sh

--- setupscripts.sh
+++ setupscripts.sh
@@ -28,7 +28,7 @@ chmod a+x /opt/sqlpackage/sqlpackage
 echo "deploy sql scripts"
 echo $AZURESQLDB_CONN_STR
 
-/opt/sqlpackage/sqlpackage /Action:Publish /SourceFile:"./fix.me.sql.dacpac" /TargetConnectionString:"$AZURESQLDB_CONN_STR" 2>&1 | tee $AZ_SCRIPTS_OUTPUT_PATH
+/opt/sqlpackage/sqlpackage /Action:Publish /SourceFile:"./fix.me.sql.dacpac" /TargetConnectionString:$AZURESQLDB_CONN_STR 2>&1 | tee $AZ_SCRIPTS_OUTPUT_PATH
 
 echo "create appsettings.json file with sql connection string"
 sed -i "s/PLACEHOLDER/$AZURESQLDB_CONN_STR/" ./appsettings.json
