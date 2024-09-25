05353c52e282ecab665490084667440ed8837ea6
Divya Bhuvanapalli <49796596+dbhuvanapalli@users.noreply.github.com>
2024-09-25 08:49:03 -0700

    Update setupscripts.sh

--- setupscripts.sh
+++ setupscripts.sh
@@ -3,13 +3,18 @@
 rgName="$1"
 prefix="$2"
 clusterName="$3"
+msiResoruceId="$4"
+
+az login --identity --username $msiResoruceId
 
 echo "executing setup scripts"
 
-apt-get update && apt-get -y install dpkg
-apt-get install -y wget unzip
+apk update 
+apk add wget unzip curl dpkg dotnet-sdk-6.0 aspnetcore-runtime-6.0
+#apk add ca-certificates libc6 libgcc-s1 libgssapi-krb5-2 libicu70 liblttng-ust1 libssl3 libstdc++6 libunwind zlib1g
+apk add ca-certificates gcc libssl3 libunwind icu-libs icu-dev
 
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+#export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=0
 
 echo "installing kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -17,17 +22,22 @@ chmod +x ./kubectl
 mv ./kubectl /usr/local/bin
 
 echo "installing sqlpackage"
-wget -q http://snapshot.debian.org/archive/debian/20190501T215844Z/pool/main/g/glibc/multiarch-support_2.28-10_amd64.deb && dpkg -i multiarch-support*.deb
-wget -q http://snapshot.debian.org/archive/debian/20170705T160707Z/pool/main/o/openssl/libssl1.0.0_1.0.2l-1%7Ebpo8%2B1_amd64.deb && dpkg -i libssl1.0.0*.deb 
-wget -q http://ftp.us.debian.org/debian/pool/main/i/icu/libicu67_67.1-7_amd64.deb && dpkg -i libicu67_67.1-7_amd64.deb 
-wget -q -O sqlpackage.zip https://go.microsoft.com/fwlink/?linkid=2113331 
-unzip -qq sqlpackage.zip -d /opt/sqlpackage  
-chmod a+x /opt/sqlpackage/sqlpackage 
+apk add dotnet-sdk-6.0 aspnetcore-runtime-6.0
+dotnet tool install -g microsoft.sqlpackage
+export PATH="$PATH:/root/.dotnet/tools"
+
+# wget -q http://snapshot.debian.org/archive/debian/20190501T215844Z/pool/main/g/glibc/multiarch-support_2.28-10_amd64.deb && dpkg -i multiarch-support*.deb
+# wget -q http://snapshot.debian.org/archive/debian/20170705T160707Z/pool/main/o/openssl/libssl1.0.0_1.0.2l-1%7Ebpo8%2B1_amd64.deb && dpkg -i libssl1.0.0*.deb 
+# wget -q http://ftp.us.debian.org/debian/pool/main/i/icu/libicu67_67.1-7_amd64.deb && dpkg -i libicu67_67.1-7_amd64.deb 
+
+# wget -q -O sqlpackage.zip https://go.microsoft.com/fwlink/?linkid=2113331 
+# unzip -qq sqlpackage.zip -d /opt/sqlpackage  
+# chmod a+x /opt/sqlpackage/sqlpackage 
 
 echo "deploy sql scripts"
 echo $AZURESQLDB_CONN_STR
 
-/opt/sqlpackage/sqlpackage /Action:Publish /SourceFile:"./fix.me.sql.dacpac" /TargetConnectionString:"$AZURESQLDB_CONN_STR2" 2>&1 | tee $AZ_SCRIPTS_OUTPUT_PATH
+sqlpackage /Action:Publish /SourceFile:"./fix.me.sql.dacpac" /TargetConnectionString:"$AZURESQLDB_CONN_STR" 2>&1 | tee $AZ_SCRIPTS_OUTPUT_PATH
 
 echo "create appsettings.json file with sql connection string"
 sed -i "s/PLACEHOLDER/$AZURESQLDB_CONN_STR/" ./appsettings.json
